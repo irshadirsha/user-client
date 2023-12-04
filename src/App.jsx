c4220d3cd8217f659164ace041d70709ab08b0d2
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-12-04 19:27:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import SignupPage from './pages/SignupPage'
+
+function App() {
+
+  return (
+    <>
+      <div>
+    <SignupPage/>
+      </div>
+    </>
+  )
+}
+
+export default App
